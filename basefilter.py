3d77e64d28a482d1d680b58092403e0a656e0100
Duff <59646857+apizdose@users.noreply.github.com>
2021-10-26 11:35:43 +0300

    Add files via upload

---
+++ basefilter.py
@@ -0,0 +1,62 @@
+import csv
+
+base=input('your base: ') or 'base'
+   
+name=''
+name=name+'\n'.lower()
+capcase = name.capitalize() 
+lowcase = name.lower()
+
+symarr=['_','.',]
+for i in range(10):
+    symarr.append(str(i))
+   
+
+blacklist=[]
+nolist=[]
+mybase=[]
+
+
+
+
+with open("blacklist.txt", "r", encoding='utf-8') as file:
+    global tname
+    for i in file:
+        tname = i.lower().replace("\n", "")
+        if len(tname)>=5:
+            blacklist.append(tname)
+        if len(tname)<5:
+            for i in symarr:
+                addname = tname+i
+                blacklist.append(addname)
+                
+        
+
+with open(f'{base}.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for name in reader:
+        mybase.append(name[0])
+
+for name in mybase:
+    name=name.lower()
+    for i in blacklist:
+              
+            if i in name:
+               nolist.append(name)
+    
+
+for i in nolist:
+    try:mybase.remove(i)
+    except:continue
+
+for i in mybase:
+    if 'raja' in i or 'rama' in i:
+        mybase.remove(i)
+
+with open(f'{base}_redacted.csv', mode='a',newline='') as mkf:
+    mkwriter = csv.writer(mkf)
+    for i in mybase:
+        mkwriter.writerow([i.replace("\n", "")])
+
+
+  
